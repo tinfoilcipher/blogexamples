d22b606a177548b9d4fbe0dbcfa0dc0ab14ca584
welsh <welsh@madcaplaughs.co.uk>
2022-05-14 14:29:08 +0100

    Updated localstack provider

--- localstack-terraform-example/provider.tf
+++ localstack-terraform-example/provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-
+    
     #--Standard Provider Configurations
     region     = "eu-west-1"
     default_tags {
@@ -7,7 +7,7 @@ provider "aws" {
             Environment = "dev"
         }
     }
-
+    
     #--Mocked Credentials
     access_key = "test"
     secret_key = "test"
@@ -21,30 +21,16 @@ provider "aws" {
     skip_metadata_api_check     = true
     skip_requesting_account_id  = true
     
-    #--Redirect Service Endpoints to Localstack. Whilst we won't be using all of these
-    #--these endpoints, they should all be configured to avoid rogue resource creation!
+    #--Redirect Service Endpoints to Localstack. Whilst we won't be any of these it's good
+    #--to see how they work and one should be specified to avoid rogue creations.
+    #--See the Localstack docs for a full list of suitable endpoints):
+    #--https://registry.terraform.io/providers/hashicorp/aws/latest/docs/guides/custom-service-endpoints#available-endpoint-customizations
     endpoints {
         apigateway     = "http://localhost:4566"
-        cloudformation = "http://localhost:4566"
         cloudwatch     = "http://localhost:4566"
-        dynamodb       = "http://localhost:4566"
         ec2            = "http://localhost:4566"
-        es             = "http://localhost:4566"
         elasticache    = "http://localhost:4566"
-        firehose       = "http://localhost:4566"
-        iam            = "http://localhost:4566"
-        kinesis        = "http://localhost:4566"
-        lambda         = "http://localhost:4566"
-        rds            = "http://localhost:4566"
-        redshift       = "http://localhost:4566"
         route53        = "http://localhost:4566"
         s3             = "http://s3.localhost.localstack.cloud:4566" #--This format required for s3_force_path_style = true
-        secretsmanager = "http://localhost:4566"
-        ses            = "http://localhost:4566"
-        sns            = "http://localhost:4566"
-        sqs            = "http://localhost:4566"
-        ssm            = "http://localhost:4566"
-        stepfunctions  = "http://localhost:4566"
-        sts            = "http://localhost:4566"
     }
 }
